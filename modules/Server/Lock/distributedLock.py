2665531a050880f7a1411ecbd4e0b44e3b141629
Ben Carothers <benjamin.carothers13@ncf.edu>
2015-11-20 17:39:38 -0500

    Could probably use some help, but it's working
    The previously mentioned bugs aren't an issue anymore it seem.

--- modules/Server/Lock/distributedLock.py
+++ modules/Server/Lock/distributedLock.py
@@ -1,3 +1,14 @@
+# ------------------------------------------------------------------------------
+#  Follows the process and rules as outlined by
+#  the slides of Petru Eles and Ivan Ukhov from Linkoping University
+#
+#  These can be found at this link:
+#  http://www.ida.liu.se/~TDDD25/lecture-notes/lect6-7.frm.pdf
+#
+#
+#  @Sarah - see the method's comments for specific rules
+# ------------------------------------------------------------------------------
+
 NO_TOKEN = 0
 TOKEN_PRESENT = 1
 TOKEN_HELD = 2
@@ -20,6 +31,16 @@ class DistributedLock(object):
         return dict(token)
 
     def initialize(self):
+
+        """
+        Rule for process initialization /* performed at initialization */
+        [RI1]: statePi:= NO-TOKEN for all processes Pi, exceptone single
+        process Px for which state Px:= TOKEN-PRESENT
+        [RI2]: token[k] initialized 0 for all elements k = 1 .. n.
+        request Pi[k] initialized 0 for all processes Pi and all
+        elements k = 1 .. n.
+        """
+
         self.peer_list.lock.acquire()
         try:
             first_peer = sorted(self.peer_list.get_peers().keys())[0]
@@ -62,16 +83,26 @@ class DistributedLock(object):
             self.peer_list.lock.release()
 
     def acquire(self):
+
+        """
+        /* performed whenever process Pi requests an access to the CS and
+        when it finally gets it; in particular Pi can already possess
+        the token */
+        [RA1]: if statePi = NO-TOKEN then Pi sends a request message to
+        all processes
+        the message is of the form (TPi, i), whereTPi=CPi is the value
+        of the local logical clock,and i is an identifier of Pi.
+        Pi waits until it receives the token.end if.state Pi:= TOKEN-HELD.Pi
+        enters the CS.
+        """
+
         self.peer_list.lock.acquire()
         self.time = self.time + 1
-
         if self.state is NO_TOKEN:
             self.peer_list.lock.release()
-
             for id in self.peer_list.get_peers():
                 self.peer_list.get_peers()[id].request_token(
                     self.time, self.owner.id)
-
             while self.state is not TOKEN_HELD:
                 pass
         else:
@@ -81,25 +112,49 @@ class DistributedLock(object):
                 self.peer_list.lock.release()
 
     def release(self):
-        self.peer_list.lock.acquire()
-        try:
-            self.state = TOKEN_PRESENT
-            for peer in sorted(self.peer_list.get_peers()):
-                if self.request[peer] > self.token[peer]:
-                    self.state = NO_TOKEN
-                    self.token[self.owner.id] = self.time
-                    self.peer_list.get_peers()[peer].obtain_token(
-                        self._prepare(self.token))
-                    break
-        finally:
-            self.peer_list.lock.release()
+
+        """
+        Rule for releasing a CS
+        /* performed by Pi after it finished work in a CS or when it holds a
+        token without using it and it got a request */
+        [RR1]: statePi = TOKEN-PRESENT
+        [RR2]: for k = [i+1, i+2, ..., n, 1, 2, ..., i-2, i-1]
+        doif requestPi[k] > token[k]
+        then statePi:= NO-TOKEN.
+        token[i] := CPi, the value of the local logical clock.
+        Pi sends the token to Pk.
+        break. /* leave the for loop */
+        """
+
+        if self.state is TOKEN_HELD:
+            self.peer_list.lock.acquire()
+            try:
+                self.state = TOKEN_PRESENT
+                for peer in sorted(self.peer_list.get_peers()):
+                    if self.request[peer] > self.token[peer]:
+                        self.state = NO_TOKEN
+                        self.token[self.owner.id] = self.time
+                        self.peer_list.get_peers()[peer].obtain_token(
+                            self._prepare(self.token))
+                        break
+            finally:
+                self.peer_list.lock.release()
 
     def request_token(self, time, pid):
+
+        """
+        Rule for handling incoming requests
+        /* performed by Piwhenever it received a request(TPj, j) from Pj*/
+        [RH1]: requestPi[j] := max(requestPi[j], TPj).
+        [RH2]: if state Pi= TOKEN-PRESENT then Pi
+        releases the resource (see rule RR2)
+        """
+
         self.peer_list.lock.acquire()
         try:
             self.request[pid] = time if time > self.request[pid] \
                 else self.request[pid]
-            if self.state is TOKEN_PRESENT and time > self.token[pid]:
+            if self.state is TOKEN_PRESENT:
                 self.peer_list.get_peers()[pid].obtain_token(
                     self._prepare(self.token))
                 self.state = NO_TOKEN
